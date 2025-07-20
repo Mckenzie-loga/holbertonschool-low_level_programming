24c6a43a266e922d6717e217dff9c73a71859e42
Mckenzie-loga <mckenzieloga2003@gmail.com>
2025-07-20 20:23:30 +0200

    Update 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * mul - check the code
- * @a: the first interger
- * @b: the second interger
- * Return: the answer of both intergers multiplied
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
  */
-
 int mul(int a, int b)
 {
 	return (a * b);
