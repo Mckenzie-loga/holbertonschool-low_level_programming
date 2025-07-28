2a685562f286cdf8e8e458782a381d7071829282
Mckenzie-loga <mckenzieloga2003@gmail.com>
2025-07-28 10:18:40 +0200

    Create 1-djb2.c

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function using djb2 algorithm
+ * @str: String to hash
+ * Return: Hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c;
+
+	return (hash);
+}
